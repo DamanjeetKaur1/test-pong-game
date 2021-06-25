df65d27c1803a485dd39acdba4fcacc61379cc5e
DamanjeetKaur1 <86105223+DamanjeetKaur1@users.noreply.github.com>
2021-06-25 14:48:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var playerPaddle;
+
+var computerPaddle
+
+function setup(){
+  createCanvas(400,400);
+  //player Paddle
+  playerPaddle = new Paddle();
+  computerPaddle = new Paddle();
+}
+
+function draw() {
+  //set background to white
+  background("white");
+  
+  playerPaddle.xPosition = 390;
+  playerPaddle.yPosition = mouseY;
+  
+  playerPaddle.display();
+  
+  computerPaddle.xPosition = 0
+  computerPaddle.yPosition = 165
+  computerPaddle.display();
+  //draw 
+  rect(200,200,10,10)
+}
